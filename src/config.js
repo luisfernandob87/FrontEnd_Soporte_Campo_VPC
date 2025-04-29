cb6131be52481bd96fd492cf2afd378460675305
Junior <luisfernandob87@gmail.com>
2025-04-28 20:07:46 -0600

    add features

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // Configuración de la API
-// export const API_BASE_URL = 'https://soporte-campo-backend.onrender.com/api';
-export const API_BASE_URL = 'http://localhost:4000';
+export const API_BASE_URL = 'https://backend-soporte-campo-vpc.onrender.com';
+// export const API_BASE_URL = 'http://localhost:4000';
